b1d61b70bba0487f7811050c636aaff14f46b63a
Chet Luther <chet.luther@gmail.com>
2011-09-25 17:38:11 -0500

    Add modeler, model and api unit tests.

--- ZenPacks/zenoss/RabbitMQ/tests/testCode.py
+++ ZenPacks/zenoss/RabbitMQ/tests/testCode.py
@@ -5,11 +5,26 @@
 #
 ###########################################################################
 
+import logging
+log = logging.getLogger('zen.RabbitMQ')
+
+import os
+
 from Products.Five import zcml
 
 from Products.DataCollector.ApplyDataMap import ApplyDataMap
 from Products.ZenTestCase.BaseTestCase import BaseTestCase
 from Products.ZenUtils.Utils import unused
+from Products.Zuul.interfaces.info import IInfo
+
+from ..modeler.plugins.zenoss.ssh.RabbitMQ import RabbitMQ as RabbitMQModeler
+
+
+def loadData(filename):
+    f = open(os.path.join(os.path.dirname(__file__), 'data', filename), 'r')
+    data = f.read()
+    f.close()
+    return data
 
 
 class TestCode(BaseTestCase):
@@ -29,6 +44,92 @@ class TestCode(BaseTestCase):
 
         import ZenPacks.zenoss.RabbitMQ
         zcml.load_config('configure.zcml', ZenPacks.zenoss.RabbitMQ)
+
+    def testModeler_rabbitmqctlNotFound(self):
+        modeler = RabbitMQModeler()
+        modeler_results = loadData('model_no_rabbitmqctl.txt')
+        data_maps = modeler.process(self.d, modeler_results, log)
+
+        self.assertEquals(data_maps, None)
+
+    def testModeler_notRunning(self):
+        modeler = RabbitMQModeler()
+        modeler_results = loadData('model_not_running.txt')
+        data_maps = modeler.process(self.d, modeler_results, log)
+
+        self.assertEquals(data_maps, None)
+
+    def testModeler_runningZenoss(self):
+        modeler = RabbitMQModeler()
+        modeler_results = loadData('model_running_zenoss.txt')
+        data_maps = modeler.process(self.d, modeler_results, log)
+
+        self.assertEquals(len(data_maps), 6)
+
+    def testModeler_runningOpenStack(self):
+        modeler = RabbitMQModeler()
+        modeler_results = loadData('model_running_openstack.txt')
+        data_maps = modeler.process(self.d, modeler_results, log)
+
+        self.assertEquals(len(data_maps), 6)
+
+    def _loadZenossData(self):
+        modeler = RabbitMQModeler()
+        modeler_results = loadData('model_running_zenoss.txt')
+
+        for data_map in modeler.process(self.d, modeler_results, log):
+            self.applyDataMap(self.d, data_map)
+
+    def testRabbitMQNode(self):
+        self._loadZenossData()
+
+        self.assertEquals(self.d.rabbitmq_nodes.countObjects(), 1)
+
+        info = IInfo(self.d.rabbitmq_nodes()[0])
+        self.assertEquals(info.vhostCount, 2)
+        self.assertEquals(info.exchangeCount, 23)
+        self.assertEquals(info.queueCount, 11)
+
+    def testRabbitMQVHost(self):
+        self._loadZenossData()
+
+        node = self.d.rabbitmq_nodes()[0]
+
+        self.assertEquals(node.rabbitmq_vhosts.countObjects(), 2)
+
+        info = IInfo(node.rabbitmq_vhosts._getOb('-'))
+        self.assertEquals(info.rabbitmq_node.name, 'rabbit@dev1')
+        self.assertEquals(info.exchangeCount, 7)
+        self.assertEquals(info.queueCount, 0)
+
+    def testRabbitMQExchange(self):
+        self._loadZenossData()
+
+        node = self.d.rabbitmq_nodes()[0]
+        vhost = node.rabbitmq_vhosts._getOb('-')
+        exchange = vhost.rabbitmq_exchanges._getOb('amq.default')
+
+        info = IInfo(exchange)
+        self.assertEquals(info.rabbitmq_node.name, 'rabbit@dev1')
+        self.assertEquals(info.rabbitmq_vhost.name, '/')
+        self.assertEquals(info.exchange_type, 'direct')
+        self.assertTrue(info.durable is True)
+        self.assertTrue(info.auto_delete is False)
+        self.assertEquals(info.arguments, '[]')
+
+    def testRabbitMQQueue(self):
+        self._loadZenossData()
+
+        node = self.d.rabbitmq_nodes()[0]
+        vhost = node.rabbitmq_vhosts._getOb('zenoss')
+        queue = vhost.rabbitmq_queues._getOb('zenoss.queues.zep.zenevents')
+
+        info = IInfo(queue)
+        self.assertEquals(info.rabbitmq_node.name, 'rabbit@dev1')
+        self.assertEquals(info.rabbitmq_vhost.name, '/zenoss')
+        self.assertTrue(info.durable is True)
+        self.assertTrue(info.auto_delete is False)
+        self.assertEquals(info.arguments, '[]')
 
     def testRabbitMQCTLParser(self):
         try:

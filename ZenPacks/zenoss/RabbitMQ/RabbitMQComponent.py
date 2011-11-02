08099da17d91a7bbd63051fc98247d7c78ce8270
Silas Sewell <silas@shutterstock.com>
2011-11-02 14:17:35 -0400

    Remove `event_key = "ComponentId"` because it was causing "No 'where' clause found for event_key ComponentId"

--- ZenPacks/zenoss/RabbitMQ/RabbitMQComponent.py
+++ ZenPacks/zenoss/RabbitMQ/RabbitMQComponent.py
@@ -36,8 +36,5 @@ class RabbitMQComponent(DeviceComponent, ManagedEntity):
             },),
         },)
 
-    # Query for events by id instead of name.
-    event_key = "ComponentId"
-
     # Commands are run via SSH and should not be specified absolutely.
     zCommandPath = ''
